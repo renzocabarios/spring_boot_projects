aa99cf17f6c58846540ec7a6e2d56bca1c5977d9
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-01-31 14:45:04 +0800

    Add UploadVideoRequest

---
+++ src/main/java/com/renzo/the/noob/youtubeclone/dto/UploadVideoRequest.java
@@ -0,0 +1,12 @@
+package com.renzo.the.noob.youtubeclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadVideoRequest {
+    private String userId;
+}
