56dc83708c33b10c1e03315511cedfe84b225cc2
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-01-31 13:59:20 +0800

    Generate spring boot app

---
+++ src/test/java/com/renzo/the/noob/youtubeclone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renzo.the.noob.youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
