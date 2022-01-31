653ebc5fcece811cc3a5e5f6b5716311f1bfd445
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-01-31 14:45:21 +0800

    Add UploadVideoResponse

---
+++ src/main/java/com/renzo/the/noob/youtubeclone/dto/UploadVideoResponse.java
@@ -0,0 +1,13 @@
+package com.renzo.the.noob.youtubeclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadVideoResponse {
+    private String videoId;
+    private String fileName;
+}
